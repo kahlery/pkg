6386509297321333dc46329e30672613da6201d5
KahlerYasla <garpayyasla@gmail.com>
2025-05-04 21:09:35 +0300

    Removes unnecessary JSON utility function
    Deletes a redundant function for writing JSON responses in HTTP requests

--- go/auth/middleware/nethttp/auth_middleware.go
+++ go/auth/middleware/nethttp/auth_middleware.go
@@ -1,12 +1,15 @@
 package nethttp
 
-import "net/http"
+import (
+	// Standart http package
+	"net/http"
+)
 
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
 		if authHeader == "" {
-			writeJSON(w)
+
 		}
 	})
 }
